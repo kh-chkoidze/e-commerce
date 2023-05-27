d8395ae17a2c4913b479c023f8cec43b3c575025
unknown <ilia.chkoidze12@gmail.com>
2023-05-27 15:43:10 +0400

    remove comments and unnedeed stuf

---
+++ save.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+
+import connectToMongo from "./config/mongo.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Product from "./models/product.js";
+import prompt from "prompt";
+
+dotenv.config();
+connectToMongo();
+
+async function saveProduct(productName, productPrice, productId) {
+  try {
+    const newProduct = new Product({
+      id: productId,
+      name: productName,
+      price: productPrice,
+    });
+
+    await newProduct.save();
+    console.log("Product saved successfully");
+  } catch (error) {
+    console.error("Failed to save product:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+prompt.start();
+
+prompt.get(
+  ["productName", "productPrice", "productId"],
+  async function (err, result) {
+    if (err) {
+      console.error("Prompt error:", err);
+      return;
+    }
+
+    console.log("Product Name:", result.productName);
+    console.log("Product Price:", result.productPrice);
+    console.log("Product ID:", result.productId);
+
+    await saveProduct(
+      result.productName,
+      result.productPrice,
+      result.productId
+    );
+  }
+);
