31e4dbe259ce46fe37f0d3a42b655ce22b42ce65
unknown <ilia.chkoidze12@gmail.com>
2023-05-27 12:51:33 +0400

    console takes product with prompt and saves on database

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 #! /usr/bin/env node
 
 import connectToMongo from "./config/mongo.js";
-
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import Product from "./models/product.js";
 import prompt from "prompt";
 
 dotenv.config();
 connectToMongo();
+
+async function saveProduct(productName, productPrice, productId) {
+  try {
+    const newProduct = new Product({
+      id: productId,
+      name: productName,
+      price: productPrice,
+    });
+
+    await newProduct.save();
+    console.log("Product saved successfully");
+  } catch (error) {
+    console.error("Failed to save product:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+}
 
 prompt.start();
 /*prompt.get(["name", "price", "id"], function (err, result) {
@@ -24,13 +41,18 @@ prompt.start();
 
 prompt.get(
   ["productName", "productPrice", "productId"],
-  function (err, result) {
+  async function (err, result) {
+    if (err) {
+      console.error("Prompt error:", err);
+      return;
+    }
+
     console.log("Product Name:", result.productName);
     console.log("Product Price:", result.productPrice);
     console.log("Product ID:", result.productId);
 
     // Create a new product document
-    const newProduct = new Product({
+    /*const newProduct = new Product({
       name: result.productName,
       price: result.productPrice,
       id: result.productId,
@@ -46,7 +68,12 @@ prompt.get(
 
       // Close the database connection
       mongoose.connection.close();
-    });
+    });*/
+    await saveProduct(
+      result.productName,
+      result.productPrice,
+      result.productId
+    );
   }
 );
 
